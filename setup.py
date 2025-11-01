34147db61d943a755ecdc4d958438343ea9d817f
StijnTB <stijn.blender_render@outlook.com>
2025-11-01 14:47:37 +0100

    delete setup.py file and LF to CRLF change

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(
-        "C:\\Users\\stijn\\OneDrive - Corderius College\\5e 6e jaar PWS\\PWS repository\\competition_bot_cy.pyx",
-        compiler_directives={"language_level": "3"},
-    )
-)
